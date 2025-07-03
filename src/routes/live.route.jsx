a92a3008055f005c2ecf61b77813baf66ff9abf3
royhairul <royhairulanam19@gmail.com>
2025-07-03 07:48:13 +0700

    feat: add route for live report graph in live route

--- src/routes/live.route.jsx
+++ src/routes/live.route.jsx
@@ -1,5 +1,6 @@
 import FinanceCommissionReportPage from "@/pages/finance/pages/commission-report-page";
 import FinanceDailyReportPage from "@/pages/finance/pages/daily-report-page";
+import LiveGraphReportPage from "@/pages/live/GraphicPage";
 import LivePreviewPage from "@/pages/live/PreviewPage";
 import LiveReportPage from "@/pages/live/ReportPage";
 import { Route } from "react-router-dom";
@@ -8,5 +9,6 @@ export const liveRoutes = (
   <>
     <Route path="/live/preview" element={<LivePreviewPage />} />
     <Route path="/live/report" element={<LiveReportPage />} />
+    <Route path="/live/report/graph" element={<LiveGraphReportPage />} />
   </>
 );
