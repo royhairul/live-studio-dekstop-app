73dd31532a41d3985bbc0e17fe4b4a3fe39a32b0
royhairul <royhairulanam19@gmail.com>
2025-07-03 07:48:56 +0700

    feat: change page for live report and graph to on working page

---
+++ src/pages/live/GraphicPage.jsx
@@ -0,0 +1,46 @@
+import MainLayout from "@/layouts/main-layout";
+import { DataTable } from "@/components/data-table";
+import { DataTablePinning } from "@/components/data-table-pinning";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  IconBadge,
+  IconBarrierBlock,
+  IconDots,
+  IconHelp,
+  IconIdBadge,
+  IconReportMoney,
+} from "@tabler/icons-react";
+
+export default function LiveGraphReportPage() {
+  const breadcrumbs = [
+    {
+      icon: IconIdBadge,
+      label: "Live",
+    },
+    {
+      label: "Laporan",
+      url: "/live/report",
+    },
+    {
+      label: "Grafik",
+    },
+  ];
+
+  return (
+    <MainLayout breadcrumbs={breadcrumbs}>
+      <div className="w-full h-full flex flex-col justify-center items-center gap-2">
+        <IconBarrierBlock size={128} className="text-gray-300" />
+        <p className="text-gray-300 font-semibold text-2xl">Coming soon</p>
+        <p className="text-gray-300 font-medium">This page is on working...</p>
+      </div>
+    </MainLayout>
+  );
+}
